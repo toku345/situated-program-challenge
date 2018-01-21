2046c603b1e59c7ea3b9e059bdf863f71ccba606
Fumitaka Tokumitsu <toku345@gmail.com>
2018-01-21 18:32:42 +0900

    テスト用設定アップデート

--- rest-server/spec/rails_helper.rb
+++ rest-server/spec/rails_helper.rb
@@ -24,7 +24,9 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
+# NOTE: ActiveRecord は使っているが、自動マイグレーションはさせたくないためコメントアウト
+#       ↓が実行されると`schema_migrations`テーブルのスキーマが Rails' migration で求められるものと異なるためエラーが発生
+# ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
